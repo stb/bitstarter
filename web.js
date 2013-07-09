114629df7e91150f2347bfd9854d1992e442e9c1
Stephan Bauer <sb@bauha.com>
2013-07-09 20:40:35 +0000

    changed web.js to index.html

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-var buf = new Buffer();
+var buf = new Buffer(256);
 
 buf = fs.readFileSync('index.html')
 var message = buf.toString();
