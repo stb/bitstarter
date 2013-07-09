714788cd58dd74cc3cc6eab534a65c030b443dd7
Stephan Bauer <sb@bauha.com>
2013-07-09 20:32:59 +0000

    changed web.js to index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var buf = new Buffer();
 
+buf = fs.readFileSync('index.html')
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
